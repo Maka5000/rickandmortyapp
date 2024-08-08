5d6688611c5fdf027563e70356a49c8777b94485
Maka5000 <mak.khas01@gmail.com>
2024-08-08 22:55:03 +0500

    feat: add conditional rendering inside Single Episode page

--- src/views/SingleEpisodeView.vue
+++ src/views/SingleEpisodeView.vue
@@ -6,6 +6,9 @@ import DataList from "../components/ui/DataList.vue";
 const episode = ref(null);
 const route = useRoute();
 
+const loading = ref(false);
+const error = ref(null);
+
 async function getEpisodeByID(id) {
   const response = await fetch(`https://rickandmortyapi.com/api/episode/${id}`);
   const json = await response.json();
@@ -16,10 +19,15 @@ async function getEpisodeByID(id) {
 watch(
   () => route.params.id,
   async () => {
+    loading.value = true;
+
     try {
       episode.value = await getEpisodeByID(route.params.id);
     } catch (err) {
+      error.value = err.toString();
       console.log(err);
+    } finally {
+      loading.value = false;
     }
   },
   { immediate: true }
@@ -27,12 +35,18 @@ watch(
 </script>
 
 <template>
-  <div v-if="episode" class="episode-container">
+  <div v-if="error">Error</div>
+  <v-progress-circular v-if="loading" indeterminate></v-progress-circular>
+  <div v-if="episode && !loading" class="episode-container">
     <div class="episode-info">
       <h2 class="episode-name">{{ episode.name }}</h2>
       <ul class="episode-info-list">
-        <li class="episode-list-item"><span>Episode:</span> {{ episode.episode }}</li>
-        <li class="episode-list-item"><span>Aired_date:</span> {{ episode.air_date }}</li>
+        <li class="episode-list-item">
+          <span>Episode:</span> {{ episode.episode }}
+        </li>
+        <li class="episode-list-item">
+          <span>Aired_date:</span> {{ episode.air_date }}
+        </li>
       </ul>
     </div>
     <section>
@@ -48,37 +62,37 @@ watch(
 
 <style scoped>
 .episode-container {
-    max-width: 800px;
-    margin-top: 50px;
+  max-width: 800px;
+  margin-top: 50px;
 }
 
 .episode-name {
-    font-size: 4rem;
+  font-size: 4rem;
 }
 
 .episode-info-list {
-    list-style-type: none;
-    text-align: start;
+  list-style-type: none;
+  text-align: start;
 }
 
 .episode-list-item {
-    font-size: 2rem;
+  font-size: 2rem;
 }
 
 .episode-list-item > span {
-    font-weight: 700;
+  font-weight: 700;
 }
 
 section {
-    margin-top: 25px;
+  margin-top: 25px;
 }
 
 .title {
-    font-size: 3rem;
-    border-bottom: 2px solid black;
+  font-size: 3rem;
+  border-bottom: 2px solid black;
 }
 
 :deep(.data-list) {
-    margin-top: 25px;
+  margin-top: 25px;
 }
 </style>
